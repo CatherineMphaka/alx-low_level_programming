ae5346684286637fb2bf08057f9021028e382bff
Monyaluoe Nthabiseng Mphaka <catherinemphaka@gmail.com>
2022-09-10 13:13:12 -0700

    c program that prints exactly the words

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: 0 after printing function
+ */
+int main(void)
+{
+	printf "%s\n""with proper grammer, but the outcome is a piece of art,"
+		retun (0);
+}
