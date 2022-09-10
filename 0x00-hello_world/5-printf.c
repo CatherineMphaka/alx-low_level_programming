6cb8e7c1d49e65b32c10c468e40278ac91481f4b
Monyaluoe Nthabiseng Mphaka <catherinemphaka@gmail.com>
2022-09-10 13:21:39 -0700

    C program that prints exactly the words

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main -Entry point
+ * main - Entry point
  *
- * Return: 0 after printing function
+ * Return : Always 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-		retun (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	retun (0);
 }
