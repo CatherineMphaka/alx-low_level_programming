88e3634dfa206002f8cc09560fb4c80e2bd59464
Monyaluoe Nthabiseng Mphaka <catherinemphaka@gmail.com>
2022-09-12 01:35:37 -0700

    Will i get it right?

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -Prints all single digit numbers of base 10
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	char ch = '0'
+	for(ch = '0'; ch <= '9'; ch ++)
+	putchar(ch);
+
+	putchar('\n')
+
+	return(0);
+}
+
