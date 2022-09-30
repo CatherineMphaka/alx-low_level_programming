b45204df31cfe79b15d7f551a5d4a6dc4f16243e
Monyaluoe Nthabiseng Mphaka <catherinemphaka@gmail.com>
2022-09-30 13:35:17 -0700

    task 0, creating a static library.aa containing all the functions listed in the question.

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Entry point. Searches a string for any of a set of bytes
+ * @s: input. String to check
+ * @accept: input. String to check against
+ *
+ * Return: Always 0
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			return (s);
+		}
+	s++;
+	}
+
+return ('\0');
+}
